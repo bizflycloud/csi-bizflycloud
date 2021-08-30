b31d2ef0b72cb5c25f84095ed7a7a8e10b23fa66
lmq1999 <quanlm1999@gmail.com>
2021-08-30 17:15:18 +0700

    Change description output format

--- driver/controller.go
+++ driver/controller.go
@@ -74,7 +74,7 @@ func (cs *controllerServer) CreateVolume(ctx context.Context, req *csi.CreateVol
 	}
 	volPVCName := req.GetParameters()["csi.storage.k8s.io/pvc/name"]
 	volPVCNamespace := req.GetParameters()["csi.storage.k8s.io/pvc/namespace"]
-	Description := "bke_pvc_name: " + volPVCName + " ; bke_namespaces: " + volPVCNamespace
+	Description := volPVCNamespace + "/" + volPVCName + " by csi-bizflycloud"
 	client := cs.Client
 
 	// Verify a volume with the provided name doesn't already exist for this tenant
